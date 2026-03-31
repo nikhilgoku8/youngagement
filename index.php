1744c3b37a1cd798267f8a9fc9501cbda92a8894
nikhilgoku8 <nikhilgoku8@gmail.com>
2026-03-31 19:21:16 +0530

    Changes

--- index.php
+++ index.php
@@ -101,32 +101,173 @@
 
             <div class="heading">Transform Your Brand’s Digital Presence <br>Engage. Automate. Grow</div>
 
+            <div class="info_boxes">
+                <div class="col-sm-6">
+                    <div class="img_box">
+                        <img src="images/transform.png">
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="text_box">
+                        <p>At Youngagement, we help brands drive real social media impact through storytelling, community building, and conversion-focused strategies.</p>
+                        <p>Our customized, data-driven approach connects you with the right audience and fuels smarter growth.</p>
+                        <p>Join hands with a team that blends creativity with insight to help you engage smarter, automate faster, and grow stronger.</p>
+                    </div>
+                </div>
+                <div class="clr"></div>
+            </div>
+
         </div>
     </div>
 </section>
+<!-- transform -->
 
-<!-- Slider main container -->
-<div class="swiper">
-    <!-- Additional required wrapper -->
-    <div class="swiper-wrapper">
-        <!-- Slides -->
-        <div class="swiper-slide">Slide 1</div>
-        <div class="swiper-slide">Slide 2</div>
-        <div class="swiper-slide">Slide 3</div>
+<section class="industries">
+    <div class="contain_80">
+        <div class="inner_container">
+
+            <div class="heading white">Industries & Categories We Have Worked With</div>
+
+            <div class="coverflow_slider_wrapper">
+
+                    <div class="coverflow_slider">
+                        <div class="swiper-wrapper">
+
+                            <div class="swiper-slide">
+                                <div class="item_box">
+                                    <div class="img_box">
+                                        <img src="images/grey.png">
+                                    </div>
+                                    <div class="industry_title">Industry 1</div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="item_box">
+                                    <div class="img_box">
+                                        <img src="images/grey.png">
+                                    </div>
+                                    <div class="industry_title">Industry 2</div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="item_box">
+                                    <div class="img_box">
+                                        <img src="images/grey.png">
+                                    </div>
+                                    <div class="industry_title">Industry 3</div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="item_box">
+                                    <div class="img_box">
+                                        <img src="images/grey.png">
+                                    </div>
+                                    <div class="industry_title">Industry 4</div>
+                                </div>
+                            </div>
+                            <div class="swiper-slide">
+                                <div class="item_box">
+                                    <div class="img_box">
+                                        <img src="images/grey.png">
+                                    </div>
+                                    <div class="industry_title">Industry 5</div>
+                                </div>
+                            </div>
+
+                        </div>
+                        <!-- <div class="swiper-pagination"></div> -->
+
+                        <!-- <div class="swiper-button-prev"></div>
+                        <div class="swiper-button-next"></div> -->
+                        <!-- <div class="custom-nav-prev"><i class="fas fa-chevron-left"></i></div>
+                        <div class="custom-nav-next"><i class="fas fa-chevron-right"></i></div> -->
+
+                        <!-- <div class="swiper-scrollbar"></div> -->
+                    </div>
+
+                </div>
+
+        </div>
     </div>
-    <!-- If we need pagination -->
-    <div class="swiper-pagination"></div>
+</section>
+<!-- industries -->
 
-    <!-- If we need navigation buttons -->
-    <div class="swiper-button-prev"></div>
-    <div class="swiper-button-next"></div>
+<section class="our_work">
+    <div class="contain_80">
+        <div class="inner_container">
 
-    <!-- If we need scrollbar -->
-    <div class="swiper-scrollbar"></div>
-</div>
+            <div class="heading">Our Work</div>
+
+            <div class="insta_boxes">
+                
+                <?php for($i=1; $i<=10; $i++){ ?>
+                <div class="info_box">
+                    <div class="img_box">
+                        <img src="images/grey.png">
+                    </div>
+                </div>
+                <?php } ?>
+
+            </div>
+            <!-- insta_boxes -->
+
+        </div>
+    </div>
+</section>
+<!-- our_work -->
+
+<section class="youtube_wrapper">
+    <div class="contain_80">
+        <div class="inner_container">
+
+            <div class="heading white">YouTube Channels</div>
+
+            <div class="youtube_box">
+                <a href="#">
+                    <img src="images/800x600.png">
+                </a>
+            </div>
+
+        </div>
+    </div>
+</section>
+<!-- youtube_wrapper -->
 
 </div>
 <!-- main -->
+
+<footer>
+    <div class="contain_80">
+        <div class="inner_container">
+
+            <div class="left_pane">
+                <div class="upper_sec">
+                    <div class="heading">Call to Action</div>
+                    <div class="text">
+                        <p>Ready to elevate your brand’s social media game?
+                            <br>
+                            Book your Pre-Consultation Session with us today for just Rs. 999! In this
+                            session, we’ll walk you through our proven strategies, showcase our
+                            success stories, and craft a roadmap tailored to your business goals.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="center_pane">
+                <div class="logo_box">
+                    <img src="images/logo.png">
+                </div>
+            </div>
+            <div class="right_pane">
+                <div class="form_wrapper">
+                    <form>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</footer>
 
 
 
@@ -185,40 +326,53 @@ new WOW().init();
 
 <script src="https://cdn.jsdelivr.net/npm/swiper@12/swiper-bundle.min.js"></script>
 <script>
-const swiper = new Swiper('.swiper', {
-     autoplay: {
-       delay: 5000,
-     },
-    // Optional parameters
-    // direction: 'vertical',
-    loop: true,
+var coverflow_slider = new Swiper(".coverflow_slider", {
+      slidesPerView: 3,
+      centeredSlides: true,
+      loop: true,
+      slideToClickedSlide: true,
+        navigation: {
+            // nextEl: '.swiper-button-next',
+            // prevEl: '.swiper-button-prev',
+            nextEl: '.custom-nav-next',
+            prevEl: '.custom-nav-prev',
+        }
+    });
 
-    // If we need pagination
-    // pagination: {
-    //     el: '.swiper-pagination',
-    // },
+// const swiper = new Swiper('.swiper', {
+//      autoplay: {
+//        delay: 5000,
+//      },
+//     // Optional parameters
+//     // direction: 'vertical',
+//     loop: true,
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+//     // If we need pagination
+//     // pagination: {
+//     //     el: '.swiper-pagination',
+//     // },
 
-    // And if we need scrollbar
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     // And if we need scrollbar
+//     // scrollbar: {
+//     //     el: '.swiper-scrollbar',
+//     // },
     
-    // This is for ANIMATION
-      // on: {
-      //   slideChangeTransitionStart: function () {
-      //     resetAnimations();
-      //   },
-      //   slideChangeTransitionEnd: function () {
-      //     animateActiveSlide();
-      //   }
-      // }
-});
+//     // This is for ANIMATION
+//       // on: {
+//       //   slideChangeTransitionStart: function () {
+//       //     resetAnimations();
+//       //   },
+//       //   slideChangeTransitionEnd: function () {
+//       //     animateActiveSlide();
+//       //   }
+//       // }
+// });
 
     // This is for ANIMATION
 // function resetAnimations() {
